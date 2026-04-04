dbd7a6c65a8bd681ded6b969e13eb0f67c12a9cb
ahmetmert <ahmetmert0505@gmail.com>
2026-04-04 23:44:03 +0300

    feat: OrderService'de OrderRepository ve CQRS Pattern için Command-Request, Handler, Response nesneleri oluşturuldu

---
+++ mini-commerce/order-service/Core/order-service.Application/Repositories/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using order_service.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace order_service.Application.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<List<Order>> GetAllAsync();
+        Task<Order?> GetByIdAsync(Guid id);
+        Task AddAsync(Order order);
+        void Update(Order order);
+        Task SaveChangesAsync();
+    }
+}
---
+++ mini-commerce/order-service/Infrastructure/order-service.Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using order_service.Application.Repositories;
+using order_service.Domain.Entities;
+using order_service.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace order_service.Persistence.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly OrderServiceDbContext _context;
+
+        public OrderRepository(OrderServiceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Order>> GetAllAsync() => await _context.Orders.ToListAsync();
+
+        public async Task<Order?> GetByIdAsync(Guid id) => await _context.Orders.FindAsync(id);
+
+        public async Task AddAsync(Order order) => await _context.Orders.AddAsync(order);
+
+        public void Update(Order order) => _context.Orders.Update(order);
+
+        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+    }
+}
